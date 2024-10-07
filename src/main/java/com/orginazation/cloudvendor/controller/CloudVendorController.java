f6fa9453395ed94e4d2f69593e7075272db80340
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-10-07 12:57:41 +0530

    Created CRUD API

--- src/main/java/com/orginazation/cloudvendor/controller/CloudVendorController.java
+++ src/main/java/com/orginazation/cloudvendor/controller/CloudVendorController.java
@@ -1,16 +1,34 @@
 package com.orginazation.cloudvendor.controller;
 
 import com.orginazation.cloudvendor.model.CloudVendorModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorController {
+    CloudVendorModel cloudVendor;
 
     @GetMapping("{vendorId}")
     public CloudVendorModel getCloudVendorDetails(String vendorId){
-        return  new CloudVendorModel("1","Linode","USA","9112365456");
+//        return  new CloudVendorModel("1","Linode","USA","9112365456");
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorModel(@RequestBody CloudVendorModel cloudVendor){
+        this.cloudVendor=cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorModel(@RequestBody CloudVendorModel cloudVendor){
+        this.cloudVendor=cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorModel(String vendorId){
+        this.cloudVendor=null;
+        return "Cloud Vendor Deleted Successfully";
     }
 }
